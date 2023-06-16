babfc9ac836f8afa83a2ae989768f33d68c727b7
SanUtn <105830363+SanUtn@users.noreply.github.com>
2023-06-16 08:23:21 -0300

    sacar clave de script empleados

--- src/entidad/Persona.java
+++ src/entidad/Persona.java
@@ -5,11 +5,11 @@ public class Persona {
 	private String Nombre;
 	private String Apellido;
 	private String Sexo;
-	private int CodNacionalidad;		//luego cambiar por Nacionalidad nacionalidad
+	private Nacionalidad CodNacionalidad;		
 	private String FechaNacimiento;
 	private String Direccion;
 	private String Localidad;
-	private int Provincia;				//luego cambiar por Provincia provincia
+	private Provincia Provincia;				
 	private String Email;
 	private String Telefono;
 	private boolean Estado;
@@ -17,85 +17,102 @@ public class Persona {
 	public Persona() {
 		
 	}
-	
+
 	public int getDni() {
 		return Dni;
 	}
+
 	public void setDni(int dni) {
 		Dni = dni;
 	}
+
 	public String getNombre() {
 		return Nombre;
 	}
+
 	public void setNombre(String nombre) {
 		Nombre = nombre;
 	}
+
 	public String getApellido() {
 		return Apellido;
 	}
+
 	public void setApellido(String apellido) {
 		Apellido = apellido;
 	}
+
 	public String getSexo() {
 		return Sexo;
 	}
+
 	public void setSexo(String sexo) {
 		Sexo = sexo;
 	}
-	public int getCodNacionalidad() {
+
+	public Nacionalidad getCodNacionalidad() {
 		return CodNacionalidad;
 	}
-	public void setCodNacionalidad(int codNacionalidad) {
+
+	public void setCodNacionalidad(Nacionalidad codNacionalidad) {
 		CodNacionalidad = codNacionalidad;
 	}
+
 	public String getFechaNacimiento() {
 		return FechaNacimiento;
 	}
+
 	public void setFechaNacimiento(String fechaNacimiento) {
 		FechaNacimiento = fechaNacimiento;
 	}
+
 	public String getDireccion() {
 		return Direccion;
 	}
+
 	public void setDireccion(String direccion) {
 		Direccion = direccion;
 	}
+
 	public String getLocalidad() {
 		return Localidad;
 	}
+
 	public void setLocalidad(String localidad) {
 		Localidad = localidad;
 	}
-	public int getProvincia() {
+
+	public Provincia getProvincia() {
 		return Provincia;
 	}
-	public void setProvincia(int provincia) {
+
+	public void setProvincia(Provincia provincia) {
 		Provincia = provincia;
 	}
+
 	public String getEmail() {
 		return Email;
 	}
+
 	public void setEmail(String email) {
 		Email = email;
 	}
+
 	public String getTelefono() {
 		return Telefono;
 	}
+
 	public void setTelefono(String telefono) {
 		Telefono = telefono;
 	}
-	public boolean getEstado() {
+
+	public boolean isEstado() {
 		return Estado;
 	}
+
 	public void setEstado(boolean estado) {
 		Estado = estado;
 	}
-
-	@Override
-	public String toString() {
-		return "Persona [Dni=" + Dni + ", Nombre=" + Nombre + ", Apellido=" + Apellido + ", Sexo=" + Sexo
-				+ ", CodNacionalidad=" + CodNacionalidad + ", FechaNacimiento=" + FechaNacimiento + ", Direccion="
-				+ Direccion + ", Localidad=" + Localidad + ", Provincia=" + Provincia + ", Email=" + Email
-				+ ", Telefono=" + Telefono + ", Estado=" + Estado + "]";
-	}
+	
+	
 }
